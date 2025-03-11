9e6e5fcd6b06614bc20710bce45e7aad4f5de161
noha0002 <noha0002@stud.kea.dk>
2025-03-11 20:14:30 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("menubtn").addEventListener("click", function () {
+    document.getElementById("sidenav").classList.toggle("active");
+  });
+});
